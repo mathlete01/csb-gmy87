b9b248c7c7b829be87f9b9337759c764f3d7a507
Code Sandbox <hello@codesandbox.io>
2021-06-26 23:46:20 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.da401bb8d9da051fa559b362ddca3784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2da57ee197e2fd2f4c",
+    "url": "https://mathlete01.github.io/csb-gmy87/static/css/main.db164752.chunk.css"
+  },
+  {
+    "revision": "fa2da57ee197e2fd2f4c",
+    "url": "https://mathlete01.github.io/csb-gmy87/static/js/main.a37fcbef.chunk.js"
+  },
+  {
+    "revision": "57ab1f2a3235de982c60",
+    "url": "https://mathlete01.github.io/csb-gmy87/static/js/runtime~main.ba899f15.js"
+  },
+  {
+    "revision": "d9e6befae9d8b913b925",
+    "url": "https://mathlete01.github.io/csb-gmy87/static/js/2.b97c236e.chunk.js"
+  },
+  {
+    "revision": "d7750030dd82e6c15477f409fb9dc5ec",
+    "url": "https://mathlete01.github.io/csb-gmy87/index.html"
+  }
+];
